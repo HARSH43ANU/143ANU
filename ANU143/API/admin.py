df26b1572ba328d82beae02052da0ce0ba56c76d
HARSH43ANU <113297908+HARSH43ANU@users.noreply.github.com>
2022-09-11 20:10:55 +0530

    Delete ANU143 directory

--- ANU143/API/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Post, Content, Season
-# Register your models here.
-
-admin.site.register(Post)
-admin.site.register(Season)
-admin.site.register(Content)
